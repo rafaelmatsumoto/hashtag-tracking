c1c883e61f62f6fa98d911f5f3f808d54e5f6ff1
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-12-08 23:02:34 -0300

    ✅ Update filtering tests in controller specs

--- spec/controllers/hashtags_controller_spec.rb
+++ spec/controllers/hashtags_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe HashtagsController, type: :controller do
+    let(:hashtag) { Hashtag.create(name: "#svelte") }
+
     describe "GET index" do
         it "has a 200 status code" do
             get :index
@@ -14,6 +16,18 @@ RSpec.describe HashtagsController, type: :controller do
         it "has a 204 status code" do
             delete :destroy, params: { id: hashtag.id }
             expect(response).to have_http_status(:no_content)
+        end
+    end
+
+    describe "filtering" do
+        it "returns json containing svelte object" do
+            get :index, params: { :hashtag_id => hashtag.id, :starts_with => "sve"}
+            expect(response.body).to eq([hashtag].to_json)
+        end
+
+        it "returns an empty array" do
+            get :index, params: { :hashtag_id => hashtag.id, :starts_with => "vue"}
+            expect(response.body).to eq([].to_json)
         end
     end
     
