1d041919d507af987377a8f6105d355d69f77491
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-12-09 20:19:10 -0300

    ✅ Update tweet's spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Tweet, type: :model do
   subject { 
     hashtag_id = Hashtag.create!(name: "Foo").id
-    described_class.new(text: "Foo bar", published_date: DateTime.new(2001,2,3,4,5,6), hashtag_id: hashtag_id)
+    described_class.new(text: "Foo bar", 
+                        published_date: DateTime.new(2001,2,3,4,5,6), 
+                        hashtag_id: hashtag_id,
+                        name: "Foo",
+                        username: "@foobar")
   } 
 
   it "is valid with attributes" do
