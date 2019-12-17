be8ef86463f87dc0aad4628c5993993bc26522b6
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-12-17 12:43:25 -0300

    ✅ Add specs to remaining requests

--- spec/requests/hashtags_spec.rb
+++ spec/requests/hashtags_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe 'hashtags', :type => :request do
         expect(response.content_type).to eq('application/json; charset=utf-8')
         expect(response).to have_http_status(:created)  
     end
+
+    it 'returns a json response' do
+        get '/hashtags'
+
+        expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
+
+    it 'deletes a hashtag' do
+        hashtag = Hashtag.create!(name: "#foo")
+
+        delete "/hashtags/#{hashtag.id}"
+
+        expect(response).to have_http_status(:no_content)          
+    end
 end
 
 
